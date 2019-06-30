26ca36213ccfeba24d8858d7d7a82f91218acd4d
Lishant Sahu <lishantsahu@gmail.com>
2019-06-30 11:24:05 +0530

    Create delete_duplicate2.cpp

---
+++ Linked List/delete_duplicate2.cpp
@@ -0,0 +1,13 @@
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode* p = head;
+    SinglyLinkedListNode* n = head;
+    for (p = head; p != NULL; p = p->next) {
+        while ((p->next != NULL) && (p->data == p->next->data)) {
+            n = p->next->next;
+            delete(p->next);
+            p->next = n;
+        }
+    }
+    return head;
+
+}
