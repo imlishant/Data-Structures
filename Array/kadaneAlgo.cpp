37c7957b9e41673e52cd5d9bdcb09b11b741b68b
Lishant Sahu <lishantsahu@gmail.com>
2022-05-13 10:16:08 +0530

    Create kadaneAlgo.cpp

---
+++ Array/kadaneAlgo.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        long long tmax = arr[0];
+        long long cmax = arr[0];
+        
+        for (int i = 1; i < n; i++) {
+            cmax = max((long long)arr[i], cmax + (long long)arr[i]);
+            tmax = max(tmax, cmax);
+        }
+        return tmax;
+        
+    }
+};
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
